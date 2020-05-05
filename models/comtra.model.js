378f6cc6f2e60e5af7e8057b6084a954331dc298
Gapehood <edwinshitakwa8@gmail.com>
2020-05-05 17:22:30 +0300

    first commit

---
+++ models/comtra.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    name: String,
+    email: String,
+    city: String,
+    phone: String
+}, {
+    timestamps: true
+});
+//If you use this app with a front-end that needs id field instead of _id,
+// you have to override toJSON method that map default object to a custom object
+schema.method("toJSON", function() {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+
+
+module.exports = mongoose.model('Tutorial', schema);
