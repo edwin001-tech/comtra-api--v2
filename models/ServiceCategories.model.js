2be578a4144c7c81fc7fc8dfd095ba271dfd5106
edwin001-tech <edwinshitakwa8@gmail.com>
2020-05-17 07:23:24 +0300

    added service categories

---
+++ models/ServiceCategories.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const schema = mongoose.Schema({
+    name: String,
+},
+{
+    timestamps: true
+
+})
+//If you use this app with a front-end that needs id field instead of _id,
+// you have to override toJSON method that map default object to a custom object
+schema.method("toJSON", function() {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+module.exports = mongoose.model('ServiceCategories', Schema)
