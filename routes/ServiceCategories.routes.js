f9d9558e9f05054d93e31eddb66db3ec1dac1e33
edwin001-tech <edwinshitakwa8@gmail.com>
2020-05-19 19:52:14 +0300

    added service categories routes

--- routes/ServiceCategories.routes.js
+++ routes/ServiceCategories.routes.js
@@ -1,28 +1,28 @@
 module.exports = app => {
-    const  = require("../controllers/user.controller.js");
+    const servicecategories = require("../controllers/ServiceCategories.controller.js");
     
   
   
     var router = require("express").Router();
   
     // Create a new User
-    router.post("/", users.create);
+    router.post("/", servicecategories.create);
   
     // Retrieve all users
-    router.get("/", users.findAll);
+    router.get("/", servicecategories.findAll);
     
     // Retrieve a single user with id
-    router.get("/:id", users.findOne);
+    router.get("/:id", servicecategories.findOne);
   
     // Update a user with id
-    router.put("/:id", users.update);
+    router.put("/:id", servicecategories.update);
   
     // Delete a user with id
-    router.delete("/:id", users.delete);
+    router.delete("/:id", servicecategories.delete);
   
     // Delete all users
-    router.delete("/", users.deleteAll);
+    router.delete("/", servicecategories.deleteAll);
 
-    app.use('/api/users', router);
+    app.use('/api/servicecategories', router);
     
   }
